e23698a7971777e1efd05d79cdefbf527f3f82bc
beyzaertugrull <ertugrulbeyza2@gmail.com>
2021-03-29 22:13:40 +0300

    Create CarPark.java

---
+++ Beyza ERTUĞRUL-02170201011/CarPark.java
@@ -0,0 +1,13 @@
+
+package carpark;
+
+public class CarPark {
+
+    
+    public static void main(String[] args) {
+       LogIn  lg= new LogIn();
+       lg.setVisible(true);
+            
+    }
+    
+}
